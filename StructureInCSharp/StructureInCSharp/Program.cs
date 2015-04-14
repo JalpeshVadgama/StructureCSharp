6d7393f303e69d90c3b98ccec6bbf8cdfc5b42d5
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-14 23:49:34 +0530

    Added structure code.

--- StructureInCSharp/StructureInCSharp/Program.cs
+++ StructureInCSharp/StructureInCSharp/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace StructureInCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
+            employee.FirstName = "Jalpesh";
+            employee.LastName = "Vadgama";
+            employee.Designation = "Project Manager";
+            employee.Id = 1;
+
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.FirstName);
+            Console.Write(employee.LastName);
+            Console.WriteLine(employee.Designation);
         }
+    }
+
+    struct Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Designation { get; set; }
     }
 }
