cca1ee40363028ec8c5200db53213494f77be176
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-17 00:53:12 +0530

    Added regions, changed sequence of backing fields!

--- StructureInCSharp/StructureConstructors/Program.cs
+++ StructureInCSharp/StructureConstructors/Program.cs
@@ -16,15 +16,23 @@ namespace StructureConstructors
 
     public struct Customer
     {
+        #region Fields
+
+        public int CustomerId;
+        public string FirstName;
+        public string LastName;
+
+        #endregion 
+
+        #region Constructor
+
         public Customer(int customerId, string firstName, string lastName)
         {
             CustomerId = customerId;
             FirstName = firstName;
             LastName = lastName;
         }
-        
-        public int CustomerId;
-        public string FirstName;
-        public string LastName;
+
+        #endregion
     }
 }
