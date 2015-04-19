d54b1c0520f4db1296ddb4ac2d21e63233590616
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-04-19 12:10:08 +0530

    Added console application logic

--- StructureInCSharp/StrcutureAndOperatorOverloading/Program.cs
+++ StructureInCSharp/StrcutureAndOperatorOverloading/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StrcutureAndOperatorOverloading
 {
@@ -10,6 +7,23 @@ namespace StrcutureAndOperatorOverloading
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee
+            {
+                EmployeeId=1,
+                FirstName="Jalpesh",
+                LastName="Vadgama"
+            };
+
+            Employee anotherEmployee = new Employee
+            {
+                EmployeeId=2,
+                FirstName="Vishal",
+                LastName="Vadgama"
+            };
+
+            bool isEqual = employee.Equals(anotherEmployee);
+            Console.WriteLine("Checking equal operator with Structure");
+            Console.WriteLine(isEqual);
         }
     }
 }
